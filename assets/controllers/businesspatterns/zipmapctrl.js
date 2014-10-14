b546220ce59e0439adf15831a76b2d447f76c974
cakesofwrath <beckadstein@gmail.com>
2014-10-14 06:32:31 -0400

    updated zipmapctrl

--- assets/controllers/businesspatterns/zipmapctrl.js
+++ assets/controllers/businesspatterns/zipmapctrl.js
@@ -1,4 +1,4 @@
-/*app.controller('zipmapctrl', */function ZipCtrl($scope){
+function ZipCtrl($scope){
 	var map;
 	/* Basemap Layers */
 	var mapquestOSM = L.tileLayer("http://{s}.tiles.mapbox.com/v3/am3081.h0po4e8k/{z}/{x}/{y}.png");
@@ -13,8 +13,23 @@
 
 	function zipSalary(d){
 		console.log(d.properties.geoid);
-		return '#0ff';
+		$scope.current_data.data.rows.forEach(i, function(){ //could binary search, but I'm lazy
+			if($scope.current_data.data.rows[i].f[1].v == d.properties.geoid.toString()){
+				console.log()
+				return rgb(0, 0, $scope.current_data.data.rows[i].f[0].v/79);	
+			}
+
+		});
+		//switch(d.properties.geoid.toString().substring(0, 2)){}
+		
+		//return '#0ff';
 	}
+	d3.json('/zipData')
+		.post(function(err,data){
+			$scope.current_data = data;		
+			console.log(data);
+			//d3.scale().linear().domain().range([#f00,#00f])
+	})
 	d3.json('/geo/ny.json',function(err,zips){
   
 	  	var options = {
@@ -26,10 +41,5 @@
 
 	});
 
-	d3.json('/zipData')
-		.post(function(err,data){
-			$scope.current_data = data;		
-			console.log(data);
-			//d3.scale().linear().domain().range([#f00,#00f])
-	})
-}//);
+	
+}
